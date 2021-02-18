f804024c5b38a46dce6746c831af9adbd99b6e9e
DragoonKite <frank.depaolo@yahoo.com>
2021-02-17 23:35:23 -0600

    added Player getStats and getInventory functions

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -19,17 +19,17 @@ test("gets player's stats as an Object", () =>{
     const player = new Player('Dave');
 
     expect(player.getStats()).toHaveProperty('potions');
-    expect(player.getStats()).toHaveProperty('healths');
+    expect(player.getStats()).toHaveProperty('health');
     expect(player.getStats()).toHaveProperty('strength');
     expect(player.getStats()).toHaveProperty('agility');
 });
 
 test('gets inventory from player or returns false', () =>{
-    const Player = new Player('Dave');
+    const player = new Player('Dave');
 
     expect(player.getInventory()).toEqual(expect.any(Array));
 
     player.inventory = [];
 
-    expect(player.getInventory()).toEqual('false');
+    expect(player.getInventory()).toEqual(false);
 });
